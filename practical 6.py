18fffd7f6b596ec4cfd4acd6899a58b192552b91
bharatbhati1234 <61960953+bharatbhati1234@users.noreply.github.com>
2021-08-29 11:34:39 +0530

    Update practical 6.py

--- practical 6.py
+++ practical 6.py
@@ -1,52 +1,39 @@
-class Sorting:
-    
-    def __init__(self,lst):
-        self.lst = lst
-        
-    def bubble_sort(self,lst):
-        for i in range(len(lst)):
-            for j in range(len(lst)):
-                if lst[i] < lst[j]:
-                    lst[i],lst[j] = lst[j],lst[i]
-                else:
-                    pass
-        return lst
+list1 = [74,8,81,45,76,79,0,-45,3,27,1,2,34,56]
+print("List = ",list1)
+n = len(list1)
+def bubbleSort():
+    print("Bubble Sorting")
+    for i in range(n-1): 
+        for j in range(0, n-i-1): 
+            if list1[j] > list1[j+1] : 
+                list1[j], list1[j+1] = list1[j+1], list1[j] 
+    print(list1)
 
-    def selection_sort(self,lst):
-        for i in range(len(lst)):
-            smallest_element = i
-            for j in range(i+1,len(lst)):
-                if lst[smallest_element] > lst[j]:
-                    smallest_element = j
-            lst[i],lst[smallest_element] = lst[smallest_element],lst[i]
-        return lst
+def SelectionSort():
+	print("Selection Sorting")
+	for i in range(n):
+		for j in range(i):
+			if list1[i]<list1[j]:
+				list1[i],list1[j] = list1[j],list1[i]
+	print(list1)
+	
+def InsertionSort():
+    print("Insertion Sorting")
+    for i in range(1, n): 
+        c = list1[i] 
+        j = i-1
+        while j >=0 and c < list1[j] : 
+                list1[j+1] = list1[j] 
+                j -= 1
+        list1[j+1] = c
+    print(list1)
 
-    def insertion_sort(self,lst): 
-        for i in range(1, len(lst)): 
-            index = lst[i] 
-            j = i-1
-            while j >= 0 and index < lst[j] : 
-                    lst[j + 1] = lst[j] 
-                    j -= 1
-            lst[j + 1] = index 
-        return lst
-    
-    def run_sort(self):
-        while True:
-            print('Select the sorting algorithm:')
-            print('1. Bubble Sort.')
-            print('2. Selection Sort.')
-            print('3. Insertion Sort.')
-            print('4. Quit')
-            opt = int(input('Option: '))
-            if opt == 1:
-                print(sort.bubble_sort(self.lst))
-            elif opt == 2:
-                print(sort.selection_sort(self.lst))
-            elif opt == 3:
-                print(sort.insertion_sort(self.lst))
-            else:
-                break
-lst = [12,43,53,2,5,98,57,87,12] 
-sort = Sorting(lst)
-sort.run_sort()
+inp = input("Enter (B) for Bubble Sort, (S) for selection Sort and (I) for Insertion Sort \n")
+if inp=="B" or inp=="b":
+	bubbleSort()
+elif inp=="S" or inp=="s":
+	SelectionSort()
+elif inp=="I" or inp=="i":
+	InsertionSort()
+else:
+	print("Invalid input")
