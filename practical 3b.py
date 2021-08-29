cc20487946c78c425b4f47a79335d4f3bac1e8f1
bharatbhati1234 <61960953+bharatbhati1234@users.noreply.github.com>
2021-08-29 11:30:50 +0530

    Update practical 3b.py

--- practical 3b.py
+++ practical 3b.py
@@ -1,21 +1,12 @@
-def TowerOfHanoi(n , from_rod, to_rod, aux_rod): 
-
-    if n == 1: 
-
-        print ("Move disk 1 from rod") ,from_rod,"to rod",to_rod 
-
+def hanoi(disks, source, auxiliary, target):
+    if disks == 1:
+        print('Move disk 1 from peg {} to peg {}.'.format(source, target))
         return
-
-    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod) 
-
-    print ("Move disk"),n,"from rod",from_rod,"to rod",to_rod 
-
-    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod) 
-
-          
-# Driver code 
-
-n = 4
-
-TowerOfHanoi(n, 'A', 'C', 'B')  
-# A, C, B are the name of rods 9
+ 
+    hanoi(disks - 1, source, target, auxiliary)
+    print('Move disk {} from peg {} to peg {}.'.format(disks, source, target))
+    hanoi(disks - 1, auxiliary, source, target)
+ 
+ 
+disks = int(input('Enter number of disks: '))
+hanoi(disks, 'A', 'B', 'C')
